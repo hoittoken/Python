91e30c38ef47ad768316998bca1687e2e446cfef
Mike Aubakirov <aubakirovma@gmail.com>
2023-10-03 08:37:33 +0700

    minor ups

--- Py/DS_prod/DS_prod_1/server.py
+++ Py/DS_prod/DS_prod_1/server.py
@@ -1,9 +1,12 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, request
 
+app = Flask(__name__)
 @app.route('/hello')
+
 def hello_func():
-    return 'hello!'
+    name = request.args.get('name')
+    return f'hello {name}!'
 
 if __name__ == '__main__':
+
     app.run('localhost', 5000)
